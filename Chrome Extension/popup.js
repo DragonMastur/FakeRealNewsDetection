9a582894239325c3629a9bbd75f298ae3db3df4b
DragonMastur <preston@hagerfamily.com>
2017-02-20 20:17:45 -0800

    More Chrome extension work.

--- Chrome Extension/popup.js
+++ Chrome Extension/popup.js
@@ -1,7 +1,24 @@
 var toggle_type = "off";
 
+function get_toggle_type() {
+    try {
+        chrome.storage.sync.get("seml_toggle_type", function(o) {
+            toggle_type = o."seml_toggle_type"
+        });
+    } catch(err) {
+        toggle_type = "off";
+        save_toggle_type();
+    }
+}
+
+function save_toggle_type() {
+    chrome.storage.sync.set({"seml_toggle_type": toggle_type}, function() {
+        console.log("Saved toggle type.");
+    });
+}
+
 function update_toggle() {
-    $("#toggle-detection").val("Turn " + toggle_type + " detection for this site.");
+    $("#toggle-detection").val("Turn " + toggle_type + " detection for all sites.");
 }
 
 $("#toggle-detection").click(function() {

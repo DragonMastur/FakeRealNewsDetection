4b3c172010d827e6c10b1ce55948b2968b120d80
DragonMastur <preston@hagerfamily.com>
2017-02-26 21:09:53 -0800

    Finished calculation of sites in 'seml_logic.py' and did some testing.

--- PiConsole.py
+++ PiConsole.py
@@ -4,6 +4,13 @@ import sys
 class PiConsole:
     def __init__(self, name="Pi Console"):
         self.name = name
+        self.filename = "log.txt"
+
+    def configure(self, **kwargs):
+        if kwargs != None:
+            for key, value in kwargs.iteritems():
+                if key == "filename":
+                    self.filename = value
 
     def printf(self, text, end="\n", name=None):
         if name == None:
@@ -17,3 +24,13 @@ class PiConsole:
     def logerror(self, text, end="\n"):
         time = datetime.datetime.now().isoformat()
         self.printf(text, end, "ERROR "+time+" "+self.name)
+
+    def filelog(self, text, end="\n"):
+        try:
+            with open(self.filename, 'rb') as f_in:
+                f_cont = str(f_in.read())
+        except:
+            f_cont = ""
+        f_cont += text + end
+        with open(self.filename, 'w') as f_out:
+            f_out.write(f_cont)

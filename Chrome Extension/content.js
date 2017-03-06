afe9d4885a6e241710d967956a7603becc971f64
DragonMastur <preston@hagerfamily.com>
2017-03-06 08:11:15 -0800

    Update content.js

--- Chrome Extension/content.js
+++ Chrome Extension/content.js
@@ -5,3 +5,7 @@ chrome.runtime.sendMessage({"name": "content.js", "command": "send-url"}, functi
 chrome.runtime.sendMessage({"name": "content.js", "command": "get-storage", "key": "seml_sites"}, function(response) {
     console.log(response);
 });
+
+chrome.runtime.sendMessage({"name": "content.js", "command": "get-storage", "key": "seml_options"}, function(response) {
+    console.log(response);
+});
